ac09bc51776cc65c9ee099e7cac002806be0e40e
Pranavan94 <pranavan94c@hotmail.com>
2026-03-05 16:53:58 +0100

    Start project

---
+++ src/test/kotlin/com/finance/saving_planner/SavingPlannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.finance.saving_planner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SavingPlannerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
